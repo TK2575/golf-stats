d32922697d62120ef6c03e27d6fcf8ad5b3ad833
Tom <tom.kain@gmail.com>
2024-03-12 21:47:41 -0700

    Simple standup for redis service and test

---
+++ src/test/java/dev/tk2575/golfstats/details/redis/RedisServiceTest.java
@@ -0,0 +1,31 @@
+package dev.tk2575.golfstats.details.redis;
+
+import com.github.fppt.jedismock.RedisServer;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+class RedisServiceTest {
+  
+  private RedisService svc;
+  
+  @BeforeEach
+  void setUp() throws IOException {
+    var server = RedisServer.newRedisServer().start();
+    log.info("Redis server started on host {}", server.getHost());
+    log.info("Redis server started on port {}", server.getBindPort());
+    this.svc = new RedisService(server.getHost(), server.getBindPort());
+  }
+  
+  @Test
+  void testSimpleSetAndGet() {
+    svc.set("key", "value");
+    assertEquals("value", svc.get("key"));
+  }
+
+}
