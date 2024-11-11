9608abdf4c4aa67050ef7e86bda8ef9c815eace3
Tom <tom.kain@gmail.com>
2024-11-10 21:36:09 -0800

    fix tests

--- src/test/java/dev/tk2575/golfstats/GolfStatsApplicationTests.java
+++ src/test/java/dev/tk2575/golfstats/GolfStatsApplicationTests.java
@@ -9,7 +9,7 @@ import static java.math.RoundingMode.HALF_UP;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(properties = {"redis.host=localhost", "redis.port=6379"})
+@SpringBootTest(properties = {"redis.host=localhost", "redis.port=6379", "notion.token=secret_token"})
 class GolfStatsApplicationTests {
 
 	@Test
